114c489249fed3197991da89d34ca40068672de6
Carlos Guagrilla <carlos.guagrilla@hotmail.com>
2022-01-30 17:25:56 -0500

    subida

---
+++ Proyeto/WebApplication1/Controllers/BoletaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoDB;
+using Proyecto.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class BoletaController : Controller
+    {
+        private readonly Conexion db;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
